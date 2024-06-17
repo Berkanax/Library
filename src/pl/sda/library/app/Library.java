ef60e8b77aa26d05d3594d27441a357705ba8a67
Adrian <adrian.mosk@outlook.com>
2024-06-17 17:25:44 +0200

    v0.5

--- src/pl/sda/library/app/Library.java
+++ src/pl/sda/library/app/Library.java
@@ -1,15 +1,20 @@
+package pl.sda.library.app;
+import pl.sda.library.model.Book;
+
 class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.3";
+        final String appName = "Biblioteka v0.5";
 
         Book book1 = new Book("Zbuntowana","Veronica Roth",2012,367, "Amber", "9788324153497");
         Book book2 = new Book("Niezgodna","Veronica Roth",2011,350, "Amber", "9788324153718");
         Book book3 = new Book("Mitologia Nordycka","Neil Gaiman",2021,227, "Mag", "9788367023658");
+        Book book4 = new Book("Wprowadzenie do javy","Y. Daniel Liang",2015,1542, "Helion");
 
         System.out.println(appName);
         System.out.println("### Książki dostepnę w bibliotece ###");
 book1.printInfo();
 book2.printInfo();
 book3.printInfo();
+book4.printInfo();
     }
 }
