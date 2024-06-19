d14875bb137b1fe81d8d6a7229eab2cd89f03837
Adrian <adrian.mosk@outlook.com>
2024-06-19 14:37:59 +0200

    v0.7

--- src/pl/sda/library/app/Library.java
+++ src/pl/sda/library/app/Library.java
@@ -3,18 +3,18 @@ import pl.sda.library.model.Book;
 
 class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.6";
-
-        Book book1 = new Book("Zbuntowana","Veronica Roth",2012,367, "Amber", "9788324153497");
-        Book book2 = new Book("Niezgodna","Veronica Roth",2011,350, "Amber", "9788324153718");
-        Book book3 = new Book("Mitologia Nordycka","Neil Gaiman",2021,227, "Mag", "9788367023658");
-        Book book4 = new Book("Wprowadzenie do javy","Y. Daniel Liang",2015,1542, "Helion");
+        final String appName = "Biblioteka v0.7";
+        Book[] books = new Book[1000];
+        books[0] = new Book("Zbuntowana","Veronica Roth",2012,367, "Amber", "9788324153497");
+        books[1] = new Book("Niezgodna","Veronica Roth",2011,350, "Amber", "9788324153718");
+        books[2] = new Book("Mitologia Nordycka","Neil Gaiman",2021,227, "Mag", "9788367023658");
+        books[3] = new Book("Wprowadzenie do javy","Y. Daniel Liang",2015,1542, "Helion");
 
         System.out.println(appName);
         System.out.println("### Książki dostepnę w bibliotece ###");
-book1.printInfo();
-book2.printInfo();
-book3.printInfo();
-book4.printInfo();
+books[0].printInfo();
+books[1].printInfo();
+books[2].printInfo();
+books[3].printInfo();
     }
 }
