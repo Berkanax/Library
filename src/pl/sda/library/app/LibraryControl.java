50c0b23df2ea046522680e007d1cb3db59d3f348
Adrian <adrian.mosk@outlook.com>
2024-06-28 15:46:14 +0200

    v1.1

--- src/pl/sda/library/app/LibraryControl.java
+++ src/pl/sda/library/app/LibraryControl.java
@@ -2,11 +2,14 @@ package pl.sda.library.app;
 import pl.sda.library.io.DataReader;
 import pl.sda.library.model.Book;
 import pl.sda.library.model.Library;
+import pl.sda.library.model.Magazine;
 
 public class LibraryControl {
     private static final int EXIT = 0;
     private static final int ADD_BOOK = 1;
-    private static final int PRINT_BOOKS = 2;
+    private static final int ADD_MAGAZIN = 2;
+    private static final int PRINT_BOOKS = 3;
+    private static final int PRINT_MAGAZIN = 4;
 
     private Library library = new Library();
     private DataReader dataReader = new DataReader();
@@ -21,8 +24,14 @@ public class LibraryControl {
                 case ADD_BOOK:
                     addBook();
                     break;
+                case ADD_MAGAZIN:
+                    addMagazine();
+                    break;
                 case PRINT_BOOKS:
                     printBooks();
+                    break;
+                case PRINT_MAGAZIN:
+                    printMagazines();
                     break;
                 case EXIT:
                     exit();
@@ -31,6 +40,16 @@ public class LibraryControl {
                     System.out.println("Nieznana komenda");
             }
         } while (option != EXIT);
+    }
+
+    private void printMagazines() {
+        library.printMagazin();
+    }
+
+    private void addMagazine() {
+        Magazine magazine = dataReader.readAndCreateMagazine();
+        library.addMagazine(magazine);
+
     }
 
     private void exit() {
@@ -53,6 +72,8 @@ public class LibraryControl {
         System.out.println("## Wybierz opcję:");
         System.out.println("."+ EXIT +". - Wyjście z programu");
         System.out.println("."+ ADD_BOOK +". - Wprowadź nową książke");
+        System.out.println("."+ ADD_MAGAZIN +". - Wprowadź nową czasopismo");
         System.out.println("."+ PRINT_BOOKS +". - Wyswietl dostepne ksiązki");
+        System.out.println("."+ PRINT_MAGAZIN +". - Wyswietl dostepne czasopisma");
     }
 }
--- src/pl/sda/library/model/Library.java
+++ src/pl/sda/library/model/Library.java
@@ -2,8 +2,11 @@ package pl.sda.library.model;
 
 public class Library {
     private static final int MAX_BOOKS = 1000;
+    private static final int MAX_MAGAZINE = 1000;
     private Book[] books = new Book[MAX_BOOKS];
+    private Magazine[] magazines = new Magazine[MAX_MAGAZINE];
     private int booksNumber = 0;
+    private int magazinesNumber = 0;
 
     public void addBook(Book book){
         if(booksNumber < MAX_BOOKS){
@@ -21,6 +24,24 @@ public class Library {
         }
         for (int i = 0; i < booksNumber; i++) {
             books[i].printInfo();
+        }
+    }
+
+    public void addMagazine(Magazine magazine){
+        if(booksNumber < MAX_MAGAZINE){
+            magazines[magazinesNumber] = magazine;
+            magazinesNumber++;
+        }else {
+            System.out.println("Brak miejsca w bibliotece");
+        }
+
+    }
+    public void printMagazin(){
+        if(magazinesNumber == 0){
+            System.out.println("Brak Czasopism");
+        }
+        for (int i = 0; i < magazinesNumber; i++) {
+            magazines[i].printInfo();
         }
     }
 
