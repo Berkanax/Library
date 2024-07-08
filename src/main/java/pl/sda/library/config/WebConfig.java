f0a211664ca43717806d86ab70ab80e5ee2db417
Adrian <adrian.mosk@outlook.com>
2024-07-08 13:12:15 +0200

    Update v2.7.2

--- src/main/java/pl/sda/library/config/WebConfig.java
+++ src/main/java/pl/sda/library/config/WebConfig.java
@@ -1,5 +1,7 @@
 package pl.sda.library.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,8 +9,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    private static final Logger logger = LoggerFactory.getLogger(WebConfig.class);
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        logger.info("Configuring resource handlers");
         registry.addResourceHandler("/static/**")
                 .addResourceLocations("classpath:/static/");
     }
